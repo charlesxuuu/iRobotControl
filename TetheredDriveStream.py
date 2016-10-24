50fb50ffee9cdc2583b72df2763ff227444c05ee
CharlesXuuu <xuchi.int@gmail.com>
2016-10-23 19:33:27 -0700

    update

--- TetheredDriveStream.py
+++ TetheredDriveStream.py
@@ -60,10 +60,15 @@
 #    
 #   3. USB camera for streaming:
 #   Install vlc by
-#   $ sudo apt-get install     
-#   it need to install video4linux2 
-#   Initiate vlc streaming from USB camera
-#   
+#   $ sudo apt-get install vlc
+#   it needs to install video4linux2 
+#   Initiate vlc streaming from USB camera: 
+#   $cvlc --no-audio v4l2:///dev/video0 \
+#   $--v4l2-width 1920 --v4l2-height 1080 \ 
+#   $--v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 \
+#   $--sout '#standard{access=http{mime=multipart/x-mixed-replace; \
+#   $boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8554/}' \ 
+#   $-I dummy 
 #
 #   TODO:
 #   
