28b9aebfab6759b26f2fd2508e8080a6e0fa5d02
root <CharlesXuuu>
2016-11-01 17:29:10 -0700

    update

--- TetheredDriveStream.py
+++ TetheredDriveStream.py
@@ -332,7 +332,8 @@ class TetheredDriveApp(Tk):
 
 if __name__ == "__main__":
     cmd = "cvlc --no-audio v4l2:///dev/video0 --v4l2-width 1920 --v4l2-height 1080 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8554/}' -I dummy"
-    shell = spur.SshShell(host,username,password)
+
+    shell = spur.SshShell(host,username,password,missing_host_key=spur.ssh.MissingHostKey.accept)
     shell.spawn(["sh","-c",cmd])
     #os.system(cmd)
     app = TetheredDriveApp()
